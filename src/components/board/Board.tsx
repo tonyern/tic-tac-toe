862e3a62ed9fa763dc452debddacd8da530f2a29
Tony <tony.d.nguyen@protonmail.com>
2021-04-01 10:44:57 -0500

    Styled button.

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -9,6 +9,14 @@ const Board = (): JSX.Element => {
     return (
         <React.Fragment>
             <Button value="1" onClick={() => doSomething()} />
+            <Button value="2" onClick={() => doSomething()} />
+            <Button value="3" onClick={() => doSomething()} />
+            <Button value="4" onClick={() => doSomething()} />
+            <Button value="5" onClick={() => doSomething()} />
+            <Button value="6" onClick={() => doSomething()} />
+            <Button value="7" onClick={() => doSomething()} />
+            <Button value="8" onClick={() => doSomething()} />
+            <Button value="9" onClick={() => doSomething()} />
         </React.Fragment>
     );
 }
