7981251912a9daa4344efc3aef12ca4f7e0dba5d
Tony <tony.d.nguyen@protonmail.com>
2021-04-01 11:29:05 -0500

    Adding game logic.

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,23 +1,22 @@
-import React from "react";
 import Button from "../button/Button";
 import "./board.css";
 
 const Board = (): JSX.Element => {
-    const doSomething = (): void => {
-        console.log("Hello")
-    }
+    const doSomething = (square: string): void => {
+        console.log(square + " was pressed");
+    };
 
     return (
         <div className="game-board">
-            <Button value="1" onClick={() => doSomething()} />
-            <Button value="2" onClick={() => doSomething()} />
-            <Button value="3" onClick={() => doSomething()} />
-            <Button value="4" onClick={() => doSomething()} />
-            <Button value="5" onClick={() => doSomething()} />
-            <Button value="6" onClick={() => doSomething()} />
-            <Button value="7" onClick={() => doSomething()} />
-            <Button value="8" onClick={() => doSomething()} />
-            <Button value="9" onClick={() => doSomething()} />
+            <Button value="1" onClick={() => doSomething("1")} />
+            <Button value="2" onClick={() => doSomething("2")} />
+            <Button value="3" onClick={() => doSomething("3")} />
+            <Button value="4" onClick={() => doSomething("4")} />
+            <Button value="5" onClick={() => doSomething("5")} />
+            <Button value="6" onClick={() => doSomething("6")} />
+            <Button value="7" onClick={() => doSomething("7")} />
+            <Button value="8" onClick={() => doSomething("8")} />
+            <Button value="9" onClick={() => doSomething("9")} />
         </div>
     );
 }
--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import Board from "../board/Board";
 
 const Game = (): JSX.Element => {
+    const handleClick = (): void => {
+
+    };
+
+    const jumpTo = (): void => {
+
+    };
+
+    const renderMoves = (): void => {
+
+    };
+
     return (
         <Board />
     );
