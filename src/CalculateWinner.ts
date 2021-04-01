7a099a04b207e3bf4e0e9554189fc313857ed3cf
Tony <tony.d.nguyen@protonmail.com>
2021-04-01 11:29:36 -0500

    Created function to calculate winner.

---
+++ src/CalculateWinner.ts
@@ -0,0 +1,30 @@
+export const calculateWinner = (squares: string[]): string | null => {
+    // All possible winning moves in Tic-Tac-Toe. Look up winning moves.
+    const lines: number[][] = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c]: number[] = lines[i];
+
+        // Checking for player move and if there is a winner.
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+
+    return null;
+}
+
+const squares = [
+    null, null, null,
+    "X", "X", "X",
+    null, null, null
+];
