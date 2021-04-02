82fce64d9e298036ae2c9e261ccf66bd73f13d56
Tony <tony.d.nguyen@protonmail.com>
2021-04-02 09:14:25 -0500

    Connected squares to game board with data.

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,22 +1,13 @@
 import Button from "../button/Button";
 import "./board.css";
 
+//@ts-ignore
 const Board = ({ squares, onClick }): JSX.Element => {
-    const doSomething = (square: string): void => {
-        console.log(square + " was pressed");
-    };
-
     return (
         <div className="game-board">
-            <Button value="1" onClick={() => doSomething("1")} />
-            <Button value="2" onClick={() => doSomething("2")} />
-            <Button value="3" onClick={() => doSomething("3")} />
-            <Button value="4" onClick={() => doSomething("4")} />
-            <Button value="5" onClick={() => doSomething("5")} />
-            <Button value="6" onClick={() => doSomething("6")} />
-            <Button value="7" onClick={() => doSomething("7")} />
-            <Button value="8" onClick={() => doSomething("8")} />
-            <Button value="9" onClick={() => doSomething("9")} />
+            {squares.map((square: string, i: number) => (
+                <Button key={i} value={square} onClick={() => onClick(i)} />
+            ))}
         </div>
     );
 }
--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -23,7 +23,7 @@ const Game = (): JSX.Element => {
     };
 
     return (
-        <Board />
+        <Board squares={board} onClick={handleClick} />
     );
 }
 
